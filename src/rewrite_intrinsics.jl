4a76517f418d8bbb8eff7cb1ff24d4f990a3e52d
SimonDanisch <sdanisch@gmail.com>
2017-09-05 13:22:48 -0400

    make constructor work

--- src/rewrite_intrinsics.jl
+++ src/rewrite_intrinsics.jl
@@ -22,6 +22,8 @@ function rewrite_expr_cu(m::LazyMethod, expr)
                 was_rewritten = _was_rewritten || args_rewritten || was_rewritten
                 return true, similar_expr(expr, [func, fargs...])
             end
+        elseif isa(expr, Symbol)
+            return true, QuoteNode(expr)
         end
         false, expr
     end)
@@ -35,9 +37,10 @@ Replaces recursively Julia Base functions which are defined as intrinsics in CUD
 Returns the resulting function and a bool indicating, wether the function was rewritten.
 """
 function rewrite_for_cudanative(f, types)
+    fsym = Symbol(f)
     # TODO cover all intrinsics
     if f in (cos, sin, tan, max)
-        cu_f = getfield(CUDAnative, Symbol(f))
+        cu_f = (args...)-> (println(f, args); f(args...))#getfield(CUDAnative, Symbol(f))
         # only if there is a method defined for the argument types
         return cu_f, true # replace it!
         #end
@@ -46,7 +49,6 @@ function rewrite_for_cudanative(f, types)
     m = LazyMethod(f, types)
     # if is a Julia intrinsic, stop
     isintrinsic(m) && return f, false
-    isa(getfunction(m), DataType) && return f, false
     # otherwise go through the source and rewrite function calls recursevely the source!
     expr = try
         expr = Sugar.sugared(m.signature..., code_typed)
@@ -70,8 +72,30 @@ function rewrite_for_cudanative(f, types)
     body, was_rewritten = rewrite_expr_cu(m, expr)
     if was_rewritten
         rewritten_func_expr = Sugar.get_func_expr(m, body, gensym(string("cu_", fsym)))
+        println(rewritten_func_expr)
         eval(rewritten_func_expr), true
     else
         f, false
     end
 end
+
+# struct Test
+#     x::Float32
+#     Test() = new(sin(1f0))
+# end
+#
+# function test_intrins_recursive(a)
+#     tan(Test().x)
+# end
+# function test_intrinsic_rewrite()
+#     a, b = 1f0, 2f0
+#     f = cos
+#     sin(a) + max(a, b) + f(a) * test_intrins_recursive(a)
+# end
+#
+# f, rewr = rewrite_for_cudanative(test_intrinsic_rewrite, ())
+# f()
+#
+# function cu_Test()
+#     return (Test)((convert)(Main.Float32, (test)(1.0f0)))::Test
+# end
