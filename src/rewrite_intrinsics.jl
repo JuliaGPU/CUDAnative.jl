191581527e80ac231d376c6d305395d9fa8a9879
Tim Besard <tim.besard@gmail.com>
2017-09-05 19:07:38 +0200

    Some clean-ups.

--- src/rewrite_intrinsics.jl
+++ src/rewrite_intrinsics.jl
@@ -1,8 +1,8 @@
 import Sugar
 using Sugar: LazyMethod, expr_type, resolve_func, similar_expr, replace_expr, getfunction, isintrinsic
 
-function rewrite_expr_cu(m::LazyMethod, expr)
-    was_rewritten = false
+function rewrite_intrinsics(m::LazyMethod, expr)
+    changed = false
     body = first(replace_expr(expr) do expr
         if isa(expr, Expr)
             args, head = expr.args, expr.head
@@ -11,43 +11,40 @@ function rewrite_expr_cu(m::LazyMethod, expr)
                 types_array = expr_type.(m, args[2:end])
                 f = resolve_func(m, func)
                 types = (types_array...,)
-                func, _was_rewritten = rewrite_for_cudanative(f, types)
+                func, call_changed = rewrite_intrinsics(f, types)
                 # function arguments have to be rewritten as well!
-                args_rewritten = false
+                args_changed = false
                 fargs = map(args[2:end]) do x
-                    rewr, rewritten = rewrite_expr_cu(m, x)
-                    args_rewritten = rewritten || args_rewritten
+                    rewr, changed = rewrite_intrinsics(m, x)
+                    args_changed = changed || args_changed
                     rewr
                 end
-                was_rewritten = _was_rewritten || args_rewritten || was_rewritten
+                changed |= call_changed || args_changed
                 return true, similar_expr(expr, [func, fargs...])
             end
         end
         false, expr
     end)
-    body, was_rewritten
+    body, changed
 end
 
-
-print_exec(f, args...) = (println(args); f(args...))
 """
 Replaces recursively Julia Base functions which are defined as intrinsics in CUDAnative
-Returns the resulting function and a bool indicating, wether the function was rewritten.
+Returns the resulting function and a bool indicating, wether the function was changed.
 """
-function rewrite_for_cudanative(f, types)
-    # TODO cover all intrinsics
+function rewrite_intrinsics(f::Function, types)
+    # rewrite the function itself
+    # TODO: cover all intrinsics
     if f in (cos, sin, tan, max)
         cu_f = getfield(CUDAnative, Symbol(f))
-        # only if there is a method defined for the argument types
-        return cu_f, true # replace it!
-        #end
+        # TODO: check if there's a method for the argument types
+        return cu_f, true
     end
-    # it's not a CUDAnative intrinsic, so now we need to check it's source for intrinsics
+
+    # get the source and rewrite static parameters
     m = LazyMethod(f, types)
-    # if is a Julia intrinsic, stop
-    isintrinsic(m) && return f, false
-    isa(getfunction(m), DataType) && return f, false
-    # otherwise go through the source and rewrite function calls recursevely the source!
+    isintrinsic(m) && return f, false                   # don't rewrite Julia intrinsics
+    isa(getfunction(m), DataType) && return f, false    # don't rewrite constructors
     expr = try
         expr = Sugar.sugared(m.signature..., code_typed)
         sparams = Sugar.get_static_parameters(m)
@@ -63,14 +60,14 @@ function rewrite_for_cudanative(f, types)
         expr
     catch e
         warn(e)
-        # TODO warn or explicitely filter out errors that are expected?
-        # E.g. it can't get the ast for some stuff like Base.cconvert(DataType, x)
         return f, false
     end
-    body, was_rewritten = rewrite_expr_cu(m, expr)
-    if was_rewritten
-        rewritten_func_expr = Sugar.get_func_expr(m, body, gensym(string("cu_", fsym)))
-        eval(rewritten_func_expr), true
+
+    # rewrite the source
+    body, changed = rewrite_intrinsics(m, expr)
+    if changed
+        changed_func_expr = Sugar.get_func_expr(m, body, gensym(string("cu_", fsym)))
+        eval(changed_func_expr), true
     else
         f, false
     end
