28f44b54a164c77eb7d4284236b557f8e86e3093
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2020-01-31 23:59:34 +0100

    Move LLVM instrincs doc to docstrings

--- docs/src/device/wmma.md
+++ docs/src/device/wmma.md
@@ -62,61 +62,19 @@ The LLVM intrinsics are subdivided in three categories: load, store and multiply
 In what follows, each of these will be discussed.
 
 ### Load matrix
-
-**Julia function:** `llvm_wmma_load_{matrix}_{layout}_{shape}_{addr_space}_stride_{elem_type}(src_addr, stride)`
-
-**Corresponding LLVM instrinsic:** `@llvm.nvvm.wmma.load.{matrix}.sync.{layout}.{shape}.{addr_space}.stride.{elem_type}`
-
-**Arguments:**
-- `src_addr`: The memory address to load from.
-- `stride`: The leading dimension of the matrix, in numbers of elements.
-
-**Placeholders:**
-- `{matrix}`: The matrix to load. Can be `a`, `b` or `c`.
-- `{layout}`: The storage layout for the matrix. Can be `row` or `col`, for row major (C style) or column major (Julia style), respectively.
-- `{shape}`: The overall shape of the MAC operation. The only valid value is `m16n16k16`.
-- `{addr_space}`: The address space of `src_addr`. Can be empty (generic addressing), `shared` or `global`.
-- `{elem_type}`: The type of each element in the matrix. Can be `f16` (half precision floating point) or `f32` (full precision floating point). Note that `f32` is only valid for the matrix ``C``.
+```@docs
+CUDAnative.llvm_wmma_load
+```
 
 ### Perform multiply-accumulate
-
-**Julia function:** `llvm_wmma_mma_{a_layout}_{b_layout}_{shape}_{d_elem_type}_{c_elem_type}(a, b, c)`
-
-**Corresponding LLVM instrinsic:** `@llvm.nvvm.wmma.mma.sync.{a_layout}.{b_layout}.{shape}.{d_elem_type}.{c_elem_type}`
-
-**Arguments:**
-- `a`: The WMMA fragment corresponding to the matrix ``A``.
-- `b`: The WMMA fragment corresponding to the matrix ``B``.
-- `c`: The WMMA fragment corresponding to the matrix ``C``.
-
-**Placeholders:**
-- `{a_layout}`: The storage layout for matrix ``A``. Can be `row` or `col`, for row major (C style) or column major (Julia style), respectively. Note that this must match the layout used in the load operation.
-- `{b_layout}`: The storage layout for matrix ``B``. Can be `row` or `col`, for row major (C style) or column major (Julia style), respectively. Note that this must match the layout used in the load operation.
-- `{shape}`: The overall shape of the MAC operation. The only valid value is `m16n16k16`.
-- `{d_elem_type}`: The type of each element in the resultant ``D`` matrix. Can be `f16` (half precision floating point) or `f32` (full precision floating point).
-- `{c_elem_type}`: The type of each element in the ``C`` matrix. Can be `f16` (half precision floating point) or `f32` (full precision floating point).
-
-!!! warning
-
-    Remember that the shape, type and layout of all operations (be it MMA, load or store) **MUST** match.
-    Otherwise, the behaviour is undefined!
+```@docs
+CUDAnative.llvm_wmma_mma
+```
 
 ### Store matrix
-
-**Julia function:** `llvm_wmma_store_d_{layout}_{shape}_{addr_space}_stride_{elem_type}(dst_addr, data, stride)`
-
-**Corresponding LLVM intrinsic:** `@llvm.nvvm.wmma.store.d.sync.{layout}.{shape}.{addr_space}.stride.{elem_type}`
-
-**Arguments:**
-- `dst_addr`: The memory address to store to.
-- `data`: The ``D`` fragment to store.
-- `stride`: The leading dimension of the matrix, in numbers of elements.
-
-**Placeholders:**
-- `{layout}`: The storage layout for the matrix. Can be `row` or `col`, for row major (C style) or column major (Julia style), respectively.
-- `{shape}`: The overall shape of the MAC operation. The only valid value is `m16n16k16`.
-- `{addr_space}`: The address space of `src_addr`. Can be empty (generic addressing), `shared` or `global`.
-- `{elem_type}`: The type of each element in the matrix. Can be `f16` (half precision floating point) or `f32` (full precision floating point).
+```@docs
+CUDAnative.llvm_wmma_store
+```
 
 ### Example
 
--- src/device/cuda/wmma.jl
+++ src/device/cuda/wmma.jl
@@ -52,6 +52,25 @@ get_addrspace_info(addr_space) = convert(Int, map_ptx_as_to_as_ty[addr_space])
 # Matrix load
 # -----------
 
+@doc """
+    llvm_wmma_load_{matrix}_{layout}_{shape}_{addr_space}_stride_{elem_type}(src_addr, stride)
+
+Wrapper around the LLVM intrinsic `@llvm.nvvm.wmma.load.{matrix}.sync.{layout}.{shape}.{addr_space}.stride.{elem_type}`.
+
+# Arguments
+- `src_addr`: The memory address to load from.
+- `stride`: The leading dimension of the matrix, in numbers of elements.
+
+# Placeholders
+- `{matrix}`: The matrix to load. Can be `a`, `b` or `c`.
+- `{layout}`: The storage layout for the matrix. Can be `row` or `col`, for row major (C style) or column major (Julia style), respectively.
+- `{shape}`: The overall shape of the MAC operation. The only valid value is `m16n16k16`.
+- `{addr_space}`: The address space of `src_addr`. Can be empty (generic addressing), `shared` or `global`.
+- `{elem_type}`: The type of each element in the matrix. Can be `f16` (half precision floating point) or `f32` (full precision floating point). Note that `f32` is only valid for the matrix ``C``.
+"""
+llvm_wmma_load() = error("Cannot call llvm_wmma_load without values for placeholders!")
+export llvm_wmma_load
+
 for mat in ["a", "b", "c"],
     layout in ["col", "row"],
     shape in ["m16n16k16"],
@@ -81,11 +100,31 @@ for mat in ["a", "b", "c"],
 
     @eval $func_name(src_addr, stride) = ccall($ccall_name, llvmcall, NTuple{$sz, $frag_ty}, (Ref{$arr_ty}, Int32), src_addr, stride)
     @eval export $func_name
+    @eval @doc (@doc llvm_wmma_load) $func_name
 end
 
 # ------------
 # Matrix store
 # ------------
+
+@doc """
+    llvm_wmma_store_d_{layout}_{shape}_{addr_space}_stride_{elem_type}(dst_addr, data, stride)
+
+Wrapper around the LLVM intrinsic `@llvm.nvvm.wmma.store.d.sync.{layout}.{shape}.{addr_space}.stride.{elem_type}`.
+
+# Arguments
+- `dst_addr`: The memory address to store to.
+- `data`: The ``D`` fragment to store.
+- `stride`: The leading dimension of the matrix, in numbers of elements.
+
+# Placeholders
+- `{layout}`: The storage layout for the matrix. Can be `row` or `col`, for row major (C style) or column major (Julia style), respectively.
+- `{shape}`: The overall shape of the MAC operation. The only valid value is `m16n16k16`.
+- `{addr_space}`: The address space of `src_addr`. Can be empty (generic addressing), `shared` or `global`.
+- `{elem_type}`: The type of each element in the matrix. Can be `f16` (half precision floating point) or `f32` (full precision floating point).
+"""
+llvm_wmma_store() = error("Cannot call llvm_wmma_store without values for placeholders!")
+export llvm_wmma_store
 
 for mat in ["d"],
     layout in ["col", "row"],
@@ -113,11 +152,37 @@ for mat in ["d"],
 
     @eval $func_name(dst_addr, data, stride) = ccall($ccall_name, llvmcall, Nothing, (Ref{$arr_ty}, $(frag_types...), Int32), dst_addr, $(frag_vars...), stride)
     @eval export $func_name
+    @eval @doc (@doc llvm_wmma_store) $func_name
 end
 
 # --------------------------
 # Matrix multiply accumulate
 # --------------------------
+
+@doc """
+    llvm_wmma_mma_{a_layout}_{b_layout}_{shape}_{d_elem_type}_{c_elem_type}(a, b, c)
+
+Wrapper around the LLVM intrinsic `@llvm.nvvm.wmma.mma.sync.{a_layout}.{b_layout}.{shape}.{d_elem_type}.{c_elem_type}`.
+
+# Arguments
+- `a`: The WMMA fragment corresponding to the matrix ``A``.
+- `b`: The WMMA fragment corresponding to the matrix ``B``.
+- `c`: The WMMA fragment corresponding to the matrix ``C``.
+
+# Placeholders
+- `{a_layout}`: The storage layout for matrix ``A``. Can be `row` or `col`, for row major (C style) or column major (Julia style), respectively. Note that this must match the layout used in the load operation.
+- `{b_layout}`: The storage layout for matrix ``B``. Can be `row` or `col`, for row major (C style) or column major (Julia style), respectively. Note that this must match the layout used in the load operation.
+- `{shape}`: The overall shape of the MAC operation. The only valid value is `m16n16k16`.
+- `{d_elem_type}`: The type of each element in the resultant ``D`` matrix. Can be `f16` (half precision floating point) or `f32` (full precision floating point).
+- `{c_elem_type}`: The type of each element in the ``C`` matrix. Can be `f16` (half precision floating point) or `f32` (full precision floating point).
+
+!!! warning
+
+    Remember that the shape, type and layout of all operations (be it MMA, load or store) **MUST** match.
+    Otherwise, the behaviour is undefined!
+"""
+llvm_wmma_mma() = error("Cannot call llvm_wmma_mma without values for placeholders!")
+export llvm_wmma_mma
 
 for a_layout in ["col", "row"],
     b_layout in ["col", "row"],
@@ -151,6 +216,7 @@ for a_layout in ["col", "row"],
 
     @eval $func_name(a, b, c) = ccall($ccall_name, llvmcall, NTuple{$d_sz, $d_frag_ty}, ($(a_types...), $(b_types...), $(c_types...)), $(a_vars...), $(b_vars...), $(c_vars...))
     @eval export $func_name
+    @eval @doc (@doc llvm_wmma_mma) $func_name
 end
 
 ################################################################################
