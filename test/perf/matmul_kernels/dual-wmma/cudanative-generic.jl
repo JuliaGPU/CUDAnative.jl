43c645fa217ae92d4a8ab74c0aed638c6d4309b7
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2020-05-30 17:55:37 -0400

    Add dual plots

---
+++ test/perf/matmul_kernels/dual-wmma/cudanative-generic.jl
@@ -0,0 +1,30 @@
+using CUDAnative, CuArrays, GPUArrays, CUDAdrv, ForwardDiff;
+
+M = parse(Int, ARGS[1]);
+N = parse(Int, ARGS[2]);
+K = parse(Int, ARGS[3]);
+
+if ARGS[4] == "FP32"
+    T = Float32;
+elseif ARGS[4] == "FP16"
+    T = Float16;
+else
+    error("Invalid type: $(ARGS[4])");
+end
+
+a_h = rand(Complex{T}, (M, K)) / sqrt(T(K));
+b_h = rand(Complex{T}, (K, N)) / sqrt(T(K));
+c_h = rand(Complex{T}, (M, N));
+
+a = CuArray(reinterpret(ForwardDiff.Dual{T,T,1}, a_h));
+b = CuArray(reinterpret(ForwardDiff.Dual{T,T,1}, b_h));
+c = CuArray(reinterpret(ForwardDiff.Dual{T,T,1}, c_h));
+d = similar(c);
+
+# warmup
+GPUArrays.generic_matmatmul!(c, b, a, T(1), T(1))
+
+# profile
+for i = 1 : 10
+    CUDAdrv.@profile GPUArrays.generic_matmatmul!(c, b, a, T(1), T(1))
+end
